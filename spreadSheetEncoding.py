58f3917bfe9728b3bd561d63d4f9fa012972e529
Mubarak <akinsolamubarak1996@gmail.com>
2021-03-11 19:19:44 +0000

    new

---
+++ spreadSheetEncoding.py
@@ -0,0 +1,10 @@
+def spreadSheetEncoding(column_string):
+    num = 0
+    count = len(column_string) - 1
+    for i in column_string:
+        num += 26 ** count * (ord(i) - ord('A') + 1)
+        count -= 1
+    return print(num)
+
+
+spreadSheetEncoding('ZZ')
