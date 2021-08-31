e5f99917d95ac9610e14678ae8a35744822cc612
Mubarak Akinsola <akinsolamubarak1996@gmail.com>
2021-08-31 16:54:33 +0100

    ...

--- spreadSheetEncoding.py
+++ spreadSheetEncoding.py
@@ -1,6 +1,7 @@
 def spreadSheetEncoding(column_string):
     num = 0
     count = len(column_string) - 1
+    print(count)
     for i in column_string:
         num += 26 ** count * (ord(i) - ord('A') + 1)
         count -= 1
