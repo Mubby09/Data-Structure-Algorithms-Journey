33833aff1b224ddfca950989423d4809663681fc
Mubarak Akinsola <akinsolamubarak1996@gmail.com>
2021-09-27 16:36:55 +0100

    permutate the elements of an array

--- permArray.py
+++ permArray.py
@@ -8,3 +8,18 @@ def permArray(perm, array):
 
 
 print(permArray([2, 0, 1, 3], ['a', 'b', 'c', 'd']))
+
+
+def permArray(perm_array, array):
+    for i in range(len(array)):
+        # while the element at perm[i], (first loop the element is 2), is not equal
+        # to index 0, (first loop), swap the current element in the array to correspond present index
+        # in perm_array
+        while perm_array[i] != i:
+            array[perm_array[i]], array[i] = array[i], array[perm_array[i]]
+            perm_array[perm_array[i]
+                       ], perm_array[i] = perm_array[i], perm_array[perm_array[i]]
+    return array
+
+
+print(permArray([2, 0, 1, 3], ['a', 'b', 'c', 'd']))
