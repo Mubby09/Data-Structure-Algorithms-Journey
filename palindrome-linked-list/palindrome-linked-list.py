cacaf4d55fdef37d6746f593e479f35132032139
Mubarak Akinsola <akinsolamubarak1996@gmail.com>
2021-12-28 16:32:56 +0000

    Time: 1100 ms (63.27%), Space: 85.3 MB (31.71%) - LeetHub

---
+++ palindrome-linked-list/palindrome-linked-list.py
@@ -0,0 +1,17 @@
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution(object):
+    def isPalindrome(self, head):
+        """
+        :type head: ListNode
+        :rtype: bool
+        """
+        vals = []
+        while head:
+            vals.append(head.val)
+            head = head.next
+        return vals == vals[::-1]
+        
