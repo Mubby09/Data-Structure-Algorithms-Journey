8536e6d68a4bef08979391529209cad290b649bd
Mubarak Akinsola <akinsolamubarak1996@gmail.com>
2022-01-05 00:46:03 +0000

    Time: 31 ms (17.81%), Space: 13.4 MB (97.63%) - LeetHub

--- 20-valid-parentheses/20-valid-parentheses.py
+++ 20-valid-parentheses/20-valid-parentheses.py
@@ -14,4 +14,11 @@ class Solution(object):
             elif left_characters == [] or lookup_list[left_characters.pop()] != character:
                 return False
         return left_characters == []
+    
+    ###################################################
+    #Essencially, the line of code below referenced to the solution above simply checks for the value 
+    # of the key in the 'lookup_list' object and compares if the character in the 's' string that is 
+    # also found in the 'lookup_list' object and if they match.
+    
+    # lookup_list[left_characters.pop()] != character:
         
