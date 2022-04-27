eca4d9baa0206cc3a3449ae8b3119ee98f464a00
Mubarak Akinsola <akinsolamubarak1996@gmail.com>
2022-04-27 10:23:49 +0100

    Time: 73 ms (60.02%), Space: 14.2 MB (54.48%) - LeetHub

---
+++ 383-ransom-note/383-ransom-note.py
@@ -0,0 +1,21 @@
+class Solution:
+    def canConstruct(self, ransomNote: str, magazine: str) -> bool:
+        charCountInR = Counter(ransomNote)
+#         charCountInR = {}
+        
+#         for i in ransomNote:
+#             if i in charCountInR:
+#                 charCountInR[i] += 1
+#             else:
+#                 charCountInR[i] = 1
+                
+        
+        for c in magazine:
+            if c in charCountInR:
+                charCountInR[c]-= 1
+                if charCountInR[c] == 0:
+                    del charCountInR[c]
+            
+                    if charCountInR == []:
+                        return True
+        return not charCountInR
