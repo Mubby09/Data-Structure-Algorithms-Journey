4381af52afb336476283a5ba9b1b9b22f8a04d22
Mubarak Akinsola <akinsolamubarak1996@gmail.com>
2022-08-23 11:40:23 +0100

    Time: 56 ms (48.18%), Space: 14 MB (50.32%) - LeetHub

---
+++ 14-longest-common-prefix/14-longest-common-prefix.py
@@ -0,0 +1,18 @@
+class Solution:
+    def longestCommonPrefix(self, strs: List[str]) -> str:
+        if strs == []:
+            return false
+        elif len(strs) == 1:
+            return strs[0]
+        else:
+            strs.sort()
+            result = ''
+            for i in range(len(strs[0])):
+                if strs[0][i] == strs[-1][i]:
+                    result += strs[0][i]
+                else:
+                    break
+            return result 
+        
+        
+        
